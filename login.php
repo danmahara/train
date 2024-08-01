06ebcb208db3580779b39a018db9b5b259d307d8
Dan Mahara <dan.mahara.0909@gmail.com>
2024-08-01 11:02:23 +0545

    added login form

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!empty($_POST)) {
+    print_r($_POST);
+}
+
+?>
+
+<form action="" method="post">
+    Email: <br>
+    <input type="text" name="email">
+
+    <br>
+    Password:
+    <br>
+    <input type="text" name="password">
+    <br>
+    <button>Login</button>
+</form>
